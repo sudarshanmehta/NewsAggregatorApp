58e02505e1e9af31ccf3224aeba4549cdba8c14d
sudarshan <sudarshan.mehtacs@gmail.com>
2024-11-17 22:11:29 -0800

    implemented share functionality

--- lib/screens/NewsPage.dart
+++ lib/screens/NewsPage.dart
@@ -3,6 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:newsaggregator/services/BookmarkService.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'CategorySelectionPage.dart';
 
 class NewsPage extends StatefulWidget {
@@ -21,11 +25,20 @@ class _NewsPageState extends State<NewsPage> {
   bool isBookmarksTabSelected = false;
   String? firebaseToken; // Firebase token stored here
 
+  // ScreenshotController for capturing the screen
+  final ScreenshotController screenshotController = ScreenshotController();
+
   @override
   void initState() {
     super.initState();
     fetchFirebaseToken();
     fetchNewsArticles();
+  }
+
+  @override
+  void dispose() {
+   // screenshotController.dispose();
+    super.dispose();
   }
 
   // Fetch Firebase token
@@ -69,7 +82,6 @@ class _NewsPageState extends State<NewsPage> {
     }
   }
 
-  // Fetch bookmarked articles from Firestore
   Future<void> fetchBookmarkedArticles() async {
     try {
       final String uid = FirebaseAuth.instance.currentUser!.uid;
@@ -86,7 +98,7 @@ class _NewsPageState extends State<NewsPage> {
           .map((doc) => doc.get('articleRef') as String)
           .toList();
 
-      // Filter the `newsArticles` list based on the article references
+      // Filter the newsArticles list based on the article references
       List<Map<String, String>> filteredArticles = newsArticles
           .where((article) => bookmarkedArticleRefs.contains(article['articleId']))
           .toList();
@@ -100,7 +112,6 @@ class _NewsPageState extends State<NewsPage> {
     }
   }
 
-
   // Bookmark an article using the BookmarkService
   Future<void> bookmarkArticle(String articleId, bool isRemove) async {
     if (firebaseToken == null) {
@@ -113,7 +124,7 @@ class _NewsPageState extends State<NewsPage> {
     final result = await widget.bookmarkService.bookmarkArticle(
       articleId,
       firebaseToken!,
-      isRemove
+      isRemove,
     );
 
     if (result) {
@@ -127,40 +138,67 @@ class _NewsPageState extends State<NewsPage> {
     }
   }
 
+  // Share functionality
+  Future<void> captureAndShareScreenshot() async {
+    try {
+      // Capture the screenshot
+      final image = await screenshotController.capture();
+      if (image == null) return;
+
+      // Save the image temporarily
+      final directory = await getTemporaryDirectory();
+      final imagePath = '${directory.path}/screenshot.png';
+      final imageFile = File(imagePath);
+      await imageFile.writeAsBytes(image);
+
+      final xFile = XFile(imagePath);
+      // Share the image
+      await Share.shareXFiles([xFile], text: 'Check out this article!');
+    } catch (e) {
+      print("Error capturing and sharing screenshot: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to share the article.')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: newsArticles.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : Stack(
-        children: [
-          GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onHorizontalDragEnd: (details) {
-              if (details.primaryVelocity! < 0) {
-                navigateToCategoriesSelectionPage(context);
-              }
-            },
-            onTap: () {
-              setState(() {
-                isMenuVisible = true;
-              });
-            },
-            child: Dismissible(
-              key: ValueKey<int>(currentIndex),
-              direction: DismissDirection.vertical,
-              onDismissed: (direction) {
-                if (direction == DismissDirection.up) {
-                  loadNextArticle();
-                } else if (direction == DismissDirection.down) {
-                  loadPreviousArticle();
+    return Screenshot(
+      controller: screenshotController, // Attach the controller
+      child: Scaffold(
+        body: newsArticles.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : Stack(
+          children: [
+            GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onHorizontalDragEnd: (details) {
+                if (details.primaryVelocity! < 0) {
+                  navigateToCategoriesSelectionPage(context);
                 }
               },
-              child: buildNewsCard(),
+              onTap: () {
+                setState(() {
+                  isMenuVisible = true;
+                });
+              },
+              child: Dismissible(
+                key: UniqueKey(),
+                direction: DismissDirection.vertical,
+                onDismissed: (direction) {
+                  if (direction == DismissDirection.up) {
+                    loadNextArticle();
+                  } else if (direction == DismissDirection.down) {
+                    loadPreviousArticle();
+                  }
+                },
+                child: buildNewsCard(),
+              ),
             ),
-          ),
-          if (isMenuVisible) buildMenuOverlay(),
-        ],
+            if (isMenuVisible) buildMenuOverlay(),
+          ],
+        ),
       ),
     );
   }
@@ -181,7 +219,7 @@ class _NewsPageState extends State<NewsPage> {
               topRight: Radius.circular(15),
             ),
             child: CachedNetworkImage(
-              imageUrl: newsArticles[currentIndex]['imageUrl']!,
+              imageUrl: newsArticles[currentIndex % newsArticles.length]['imageUrl']!,
               height: 200,
               width: double.infinity,
               fit: BoxFit.cover,
@@ -196,7 +234,7 @@ class _NewsPageState extends State<NewsPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  newsArticles[currentIndex]['title']!,
+                  newsArticles[currentIndex % newsArticles.length]['title']!,
                   style: const TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
@@ -204,7 +242,7 @@ class _NewsPageState extends State<NewsPage> {
                 ),
                 const SizedBox(height: 10),
                 Text(
-                  newsArticles[currentIndex]['description']!,
+                  newsArticles[currentIndex % newsArticles.length]['description']!,
                   style: const TextStyle(fontSize: 14),
                   maxLines: 3,
                   overflow: TextOverflow.ellipsis,
@@ -216,101 +254,9 @@ class _NewsPageState extends State<NewsPage> {
       ),
     );
   }
-  Widget buildMenuOverlay() {
-    return Positioned.fill(
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            isMenuVisible = false;
-          });
-        },
-        child: Container(
-          color: Colors.black.withOpacity(0.5),
-          child: Column(
-            children: [
-              // Updated Tab Bar
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                color: Colors.white,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    // Bookmarks Tab
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if (isBookmarksTabSelected) {
-                            isBookmarksTabSelected = false;
-                            fetchNewsArticles();
-                          } else {
-                            isBookmarksTabSelected = true;
-                            fetchBookmarkedArticles();
-                          }
-                        });
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 8),
-                        decoration: BoxDecoration(
-                          color: isBookmarksTabSelected
-                              ? Colors.blue
-                              : Colors.transparent,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Text(
-                          "Bookmarks",
-                          style: TextStyle(
-                            color: isBookmarksTabSelected
-                                ? Colors.white
-                                : Colors.grey,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10), // Spacing between tabs
-
-                    // Other Tab Example: Daily Roundups (Static for now)
-                    GestureDetector(
-                      onTap: () {
-                        // Add functionality for Daily Roundups if needed
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 8),
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Text(
-                          "Daily Roundups",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const Spacer(),
-
-              // Bottom Bar
-              buildBottomBar(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
 
   Widget buildBottomBar() {
-    bool isCurrentArticleBookmarked = newsArticles[currentIndex]['isBookmarked'] == 'true';
+    bool isCurrentArticleBookmarked = newsArticles[currentIndex % newsArticles.length]['isBookmarked'] == 'true';
 
     return Container(
       color: Colors.white,
@@ -321,7 +267,7 @@ class _NewsPageState extends State<NewsPage> {
           // Bookmark Button
           GestureDetector(
             onTap: () {
-              toggleBookmark(newsArticles[currentIndex]['articleId']!);
+              toggleBookmark(newsArticles[currentIndex % newsArticles.length]['articleId']!);
             },
             child: Container(
               padding: const EdgeInsets.all(10),
@@ -339,9 +285,8 @@ class _NewsPageState extends State<NewsPage> {
 
           // Share Button
           GestureDetector(
-            onTap: () {
-              // Add share logic here
-              print("Share button pressed");
+            onTap: () async {
+              await captureAndShareScreenshot();
             },
             child: Container(
               padding: const EdgeInsets.all(10),
@@ -363,13 +308,11 @@ class _NewsPageState extends State<NewsPage> {
 
   void toggleBookmark(String articleId) {
     setState(() {
-      // Find the article and toggle its bookmark state
       for (var article in newsArticles) {
         if (article['articleId'] == articleId) {
           bool isBookmarked = article['isBookmarked'] == 'true';
           article['isBookmarked'] = (!isBookmarked).toString();
 
-          // Optionally: Call the bookmark API here to persist the change
           if (!isBookmarked) {
             bookmarkArticle(articleId, false);
           } else {
@@ -398,6 +341,68 @@ class _NewsPageState extends State<NewsPage> {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const CategorySelectionPage()),
+    );
+  }
+
+  Widget buildMenuOverlay() {
+    return Positioned.fill(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            isMenuVisible = false;
+          });
+        },
+        child: Container(
+          color: Colors.black.withOpacity(0.5),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (isBookmarksTabSelected) {
+                            isBookmarksTabSelected = false;
+                            fetchNewsArticles();
+                          } else {
+                            isBookmarksTabSelected = true;
+                            fetchBookmarkedArticles();
+                          }
+                        });
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        decoration: BoxDecoration(
+                          color: isBookmarksTabSelected
+                              ? Colors.blue
+                              : Colors.transparent,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                          "Bookmarks",
+                          style: TextStyle(
+                            color: isBookmarksTabSelected
+                                ? Colors.white
+                                : Colors.grey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              buildBottomBar(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
