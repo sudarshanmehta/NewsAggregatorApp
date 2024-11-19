86e2a3d39da6acd915ac2cdfe9c57120170da48f
sudarshan <sudarshan.mehtacs@gmail.com>
2024-11-19 13:01:47 -0800

    updated firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:newsaggregator/screens/LoginScreen.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: "AIzaSyDcJTWXtu0W35LsnZTTzqgCynjmZLE2iIU",
-      authDomain: "news-aggregator-a4193.firebaseapp.com",
-      projectId: "news-aggregator-a4193",
-      storageBucket: "news-aggregator-a4193.firebasestorage.app",
-      messagingSenderId: "345943993270",
-      appId: "1:345943993270:web:9cffbbef1b06edbe446f17",
-      measurementId: "G-QY17KTC0L4",
-    ),
-  );
-  }else{
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp(options:
+  DefaultFirebaseOptions.currentPlatform);
   runApp(const NewsAggregatorApp());
 }
 class NewsAggregatorApp extends StatelessWidget {
